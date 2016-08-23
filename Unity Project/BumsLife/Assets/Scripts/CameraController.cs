2c88ea5c9b4492686f2c1d4e6aeab070bb491b06
MuuSStY <jamesphelandeharo@gmail.com>
2016-08-23 19:24:47 +0200

    Creados scripts PlayerController y CameraController

---
+++ Unity Project/BumsLife/Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject target;
+    public float distance;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = target.transform.position - new Vector3 (0,0, distance);
+	}
+}
---
+++ Unity Project/BumsLife/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour{
+    public float moveSpeed;
+    void Start() { }
+
+    void FixedUpdate(){
+        Move();
+    }
+
+    void Move(){
+        float xMove = Input.GetAxisRaw("Horizontal");
+        float yMove = Input.GetAxisRaw("Vertical");
+        if (xMove > 0.5f || xMove < -0.5f){
+            transform.Translate(new Vector3(xMove * moveSpeed*Time.deltaTime,0f,0f));
+        }
+        if (yMove > 0.5f || yMove < -0.5f){
+            transform.Translate(new Vector3(0f, yMove*moveSpeed * Time.deltaTime, 0f));
+        }
+
+    }
+}
+
