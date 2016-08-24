79f0bebcdf81f9fd075451dc560924a3d5f285fb
AstroFeline <vata90@gmail.com>
2016-08-24 12:55:26 +0200

    Añadida animacion del vagabundo

--- Unity Project/BumsLife/Assets/Scripts/PlayerController.cs
+++ Unity Project/BumsLife/Assets/Scripts/PlayerController.cs
@@ -4,22 +4,39 @@ using System.Collections;
 public class PlayerController : MonoBehaviour{
     public float moveSpeed;
     private bool isMoving=true;
-    void Start() { }
+    private Animator anim;
+
+    void Start() {
+        anim = this.gameObject.GetComponent<Animator>();
+    }
+
+
+    
 
     void FixedUpdate(){
 
         if (IsMoving1) Move();
+
     }
 
     void Move(){
+        this.moveSpeed = 1;
         float xMove = Input.GetAxisRaw("Horizontal");
         float yMove = Input.GetAxisRaw("Vertical");
         if (xMove > 0.5f || xMove < -0.5f){
+            moveSpeed = 1;
             transform.Translate(new Vector3(xMove * moveSpeed*Time.deltaTime,0f,0f));
         }
-        if (yMove > 0.5f || yMove < -0.5f){
+        else if (yMove > 0.5f || yMove < -0.5f){
+            moveSpeed = 1;
             transform.Translate(new Vector3(0f, yMove*moveSpeed * Time.deltaTime, 0f));
         }
+        else
+        {
+            moveSpeed = 0;
+        }
+
+        anim.SetFloat("Speed", moveSpeed);
 
     }
     
