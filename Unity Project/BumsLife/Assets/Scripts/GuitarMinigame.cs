48ba02fcf70ae3c393d237b7150002adae9ba380
Bund187 <gsiliutop@gmail.com>
2016-08-28 22:56:19 +0200

    Guitar Minigame
    Comienzo del minijuego del guitarrista. Animación hecha

---
+++ Unity Project/BumsLife/Assets/Scripts/GuitarMinigame.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuitarMinigame : MonoBehaviour {
+
+    private Animator anim;
+    private bool isGuitarOn;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+	// Update is called once per frame
+	void Update () {
+        PlayGuitar();
+    }
+
+    private void PlayGuitar()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad0))
+        {
+            isGuitarOn = !isGuitarOn;
+            print("guitarra " + isGuitarOn);
+        }
+        anim.SetBool("guitarOn", isGuitarOn);
+    }
+}
